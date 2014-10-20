b0798cb599d7662b2699fe5d95506b5780e952c2
Karola Marky <karola@pinyto.de>
2014-10-20 20:23:13 +0900

    Database Model added

--- selectionTask/models.py
+++ selectionTask/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question (models.Model):
+    person = models.CharField(max_length=30)
+    question = models.TextField()
+    correct_answer = models.BooleanField()
+    image_url = models.CharField(max_length=50)
+
+
+class Result (models.Model):
+    answer_is_correct = models.BooleanField()
+    answer_time = models.FloatField()
+    subject = models.ForeignKey(Subject, related_name='results')
+
+
+class Subject (models.Model):
+    gender = models.CharField(max_length=6)
+    age = models.IntegerField()
