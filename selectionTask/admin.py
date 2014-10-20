d366fb26dc38c5a65fc0abd4024e8dc9d0cf12b1
Karola Marky <karola@pinyto.de>
2014-10-20 20:40:51 +0900

    Database Model refined

--- selectionTask/admin.py
+++ selectionTask/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from models import Question, Result, Subject
+
+admin.site.register(Question)
+admin.site.register(Result)
+admin.site.register(Subject)
--- selectionTask/models.py
+++ selectionTask/models.py
@@ -4,16 +4,16 @@ from django.db import models
 class Question (models.Model):
     person = models.CharField(max_length=30)
     question = models.TextField()
-    correct_answer = models.BooleanField()
+    correct_answer = models.CharField(max_length=5)
     image_url = models.CharField(max_length=50)
+
+
+class Subject (models.Model):
+    gender = models.CharField(max_length=6)
+    age = models.IntegerField()
 
 
 class Result (models.Model):
     answer_is_correct = models.BooleanField()
     answer_time = models.FloatField()
     subject = models.ForeignKey(Subject, related_name='results')
-
-
-class Subject (models.Model):
-    gender = models.CharField(max_length=6)
-    age = models.IntegerField()
